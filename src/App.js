5e1cef5dd1eba8ce5b7dc58d8ee4290feb206e1b
Éric B <eric.berthe@doctolib.com>
2020-01-14 15:05:51 +0100

    Split into own file

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
 import React from "react";
 import "./App.css";
-import pdfjs from "pdfjs-dist/webpack";
-
-// Inspired by https://stackoverflow.com/questions/12921052/parsing-pdf-pages-as-javascript-images
-const pdfBlobToPngBlob = async (pdfBlob, options = {}) => {
-  const { scale = 2 } = options;
-  const url = URL.createObjectURL(pdfBlob);
-  const pdf = await pdfjs.getDocument(url).promise;
-  URL.revokeObjectURL(url);
-  const page = await pdf.getPage(1);
-
-  const viewport = page.getViewport({ scale });
-
-  const canvas = document.createElement("canvas");
-  document.body.append(canvas);
-  const context = canvas.getContext("2d");
-  canvas.height = viewport.height;
-  canvas.width = viewport.width;
-  canvas.style = `height: ${viewport.height}px; width: ${viewport.width}px`;
-
-  await page.render({ canvasContext: context, viewport }).promise;
-  return new Promise(resolve => {
-    canvas.toBlob(blob => {
-      canvas.remove();
-      resolve(blob);
-    });
-  });
-};
+import pdfToPng from "./pdfToPng";
 
 export default () => {
   const [pngUrl, setPngUrl] = React.useState();
 
   const onFileChange = async event => {
     const pdfFile = event.target.files[0];
-    const pngBlob = await pdfBlobToPngBlob(pdfFile);
+    const pngBlob = await pdfToPng(pdfFile);
     if (pngUrl) {
       URL.revokeObjectURL(pngUrl);
     }
